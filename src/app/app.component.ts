8529a02cf737d4853a5591cd8d443b31a1a36ad3
EugeniaM <maltsevae88@gmail.com>
2018-02-24 23:06:07 +0200

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  lessons = [
+    {
+      lessonComplexity: 'normal',
+      name: 'Handling Forms in Angular Apps',
+      status: 'in progress',
+      started: new Date(18, 1, 2018)
+    },
+    {
+      lessonComplexity: 'complex',
+      name: 'Angular Routing',
+      status: 'new',
+      started: new Date(25, 1, 2018)
+    },
+    {
+      lessonComplexity: 'easy',
+      name: 'Pipes',
+      status: 'in progress',
+      started: new Date(15, 1, 2018)
+    },
+    {
+      lessonComplexity: 'normal',
+      name: 'Databinding',
+      status: 'done',
+      started: new Date(5, 1, 2018)
+    }
+  ];
 }
